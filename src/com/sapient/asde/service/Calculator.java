59d812da11f2889f023ae5d32cd0ae735677e189
samkumar4 <samir.kumar@publicissapient.com>
2022-06-06 16:58:24 +0530

    Second Commit

--- src/com/sapient/asde/service/Calculator.java
+++ src/com/sapient/asde/service/Calculator.java
@@ -4,5 +4,7 @@ public class Calculator {
     public double add(double number1,double number2){
         return number1+number2;
     }
-
+    public double substraction(double number1,double number2){
+        return number1-number2;
+    }
 }
--- src/com/sapient/asde/ui/CalculatorDemo.java
+++ src/com/sapient/asde/ui/CalculatorDemo.java
@@ -6,5 +6,6 @@ public class CalculatorDemo {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("10.0+27.67 =" + calculator.add(10.0, 27.67));
+        System.out.println("50.0-27.67 =" + calculator.substraction(50.0, 27.67));
     }
     }
